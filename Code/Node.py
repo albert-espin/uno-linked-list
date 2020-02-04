b9ed14a39c56a81b6c5b4d0de30bd595bf7924ad
albert-espin <59225798+albert-espin@users.noreply.github.com>
2020-02-04 12:41:13 +0100

    Code & Report

---
+++ Code/Node.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+class Node(object):
+    
+    def __init__(self, data, next = None):
+        self.__data = data
+	self.__next = next
+	
+    def __str__(self):
+	return str(self.__data)
+    
+    def getNext(self):
+	''' Retorna l'enllaç al seguent Node '''
+	return self.__next 
+    
+    def setNext(self, next):
+	''' Canvia l'enllaç del Node '''
+	self.__next = next
+	
+    def getData(self):
+	''' Retorna el valor del Node '''
+	return self.__data 
+    
+    def setData(self, data):
+	''' Canvia el valor del node '''
+	self.__data = data
---
+++ Code/TwoWayNode.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from Node import *
+
+class TwoWayNode(Node):
+    
+    def __init__(self, data, previous = None, next = None):
+        Node.__init__(self, data, next)
+        self.__previous = previous
+        
+    def getPrevious(self):
+	''' Retorna l'enllaç al seguent Node '''
+	return self.__previous 
+    
+    def setPrevious(self, previous):
+	self.__previous = previous
+        
+    # Tots els altres metodes necessaris hereden de Node
